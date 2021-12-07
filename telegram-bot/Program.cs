43e4da86a977f37ee088a1b6cf5e0610c5a18760
KrusX <KrusX@DESKTOP-ICVFG6J>
2021-12-08 00:06:49 +0300

    Добавьте файлы проекта.

---
+++ telegram-bot/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace InrecoTelegram.Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var botClient = new TelegramBotClient("5081793184:AAEgWcQZo1LXLfeTenlqSSqAFvxvg24ShA8");
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine(
+                $"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+        }
+    }
+}
